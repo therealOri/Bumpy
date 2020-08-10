fe90b37283e85b3ce58faeae8ac1b0209d132e47
Ori <45724082+therealOri@users.noreply.github.com>
2020-08-09 19:47:23 -0600

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python bump.py
---
+++ bump.py
@@ -0,0 +1,59 @@
+import os
+import logging
+import asyncio
+import discord
+
+from discord.ext import commands
+
+client = commands.Bot(description='Disboard Auto Bumper!', command_prefix="o.")
+
+TOKEN = 'NzQyMTE1OTI0NjA1NDAzMjQ3.XzBblA.duK-rCqu5pbt1_Em2FpSSF0kX5A'
+
+
+@client.command(aliases=['working check'])
+async def ping(ctx):
+    await ctx.send(f"pong! connection speed is {round(client.latency * 1000)}ms")
+
+
+
+@client.command(aliases=['bump'])
+async def start(ctx):
+#    """Starts the timer to bump every 2 hrs"""
+    while 1:
+        bumpy = 0
+        bumpy = bumpy + 1
+        try:
+            print("Processing new bump!")
+            await ctx.send('!d bump')
+            await asyncio.sleep(7200)
+            print(f"Bump {bumpy} succesfully bumped the server with disboard!")
+        except:
+            print(f"Couldn't Process bump :( {bumpy}.")
+
+
+@client.command(aliases=['bump2'])
+async def start2(ctx):
+#    """Starts the timer to bump every 2 hrs"""
+    while 1:
+        bumpy = 0
+        bumpy = bumpy + 1
+        try:
+            print("Processing new bump2!")
+            await ctx.send('.bump')
+            await asyncio.sleep(7200)
+            print(f"Bump {bumpy} succesfully bumped the server using DSC!")
+        except:
+            print(f"Couldn't Process bump :( {bumpy}.")
+
+
+@client.event
+async def on_connect():
+    print('Bumpy Online! | Succesful connection!')
+
+
+@client.event
+async def on_ready():
+    print('All Systems ready to go!')
+
+
+client.run(TOKEN, bot=False)
