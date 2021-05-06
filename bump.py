2370c12471b1d874a9507cc726a386f922040e63
Ori <45724082+therealOri@users.noreply.github.com>
2021-05-06 11:27:52 -0600

    Code removed
    Removed:
    - Verification functions.
    - Can no longer give a role upon reaction to the verify message.

--- bump.py
+++ bump.py
@@ -56,28 +56,6 @@ async def dsc(ctx):
 
 
       
-
-#You will need to run this command again in the verification channel after restarting the bot/account so it can start the event function and wait for reactions.
-#If you can fix it so it doesn't need to be re ran again after restrt then be my guest.
-@client.command(pass_context=True)
-async def verify(ctx):
-  if ctx.author.id == your_main_account_id_here:
-    Embed = discord.Embed(title="Verification", description="React to this message to gain access to the server!", color=discord.Color.green())
-    msg = await ctx.send(embed=Embed)
-    await msg.add_reaction("✅")
-  else:
-      await ctx.send("I don't listen to you.")
-
-        
-@client.event
-async def on_reaction_add(reaction, member):
-    Channel = client.get_channel('id_of_channel_for_verification_here')
-    if reaction.message.channel.id != same_id_here_as_above:
-        return
-    if reaction.emoji == "✅":
-        role = discord.utils.get(member.guild.roles, name='ROLE_NAME_HERE_TO_GIVE_USERS_AFTER_VERIFICATION')
-        await member.add_roles(role)
-      
       
       
   
